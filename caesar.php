efa5bf06e155d32a00ef1607fc408cdd085b45b7
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-09-29 14:55:22 +0700

    create file caesar algorithm calculation

---
+++ caesar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ENDETEXT GENERATOR</title>
+    <link rel="stylesheet" href="style/index.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-container">
+            <h1><a href="index.html"><span>ENDETEXT</span>GENERATOR</a></h1>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Chaesar Chipper</a></li>
+                <li><a href="#">XOR</a></li>
+                <li><a href="#">Vignere</a></li>
+                <li><a href="#">Super Enkripsi</a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <section>
+            <p>
+                <span class="important-text">Algoritma Caesar Chipper</span>
+                <br><br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, molestias corporis ullam fugit
+                numquam, minima necessitatibus sunt dignissimos nemo repudiandae rerum exercitationem dolorum facilis
+                excepturi.
+            </p>
+        </section>
+
+    </main>
+    <footer></footer>
+    <script src="script/index.js"></script>
+</body>
+
+</html>

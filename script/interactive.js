92ffc2191cb55f973d2668cd7a65d28ee8a774ba
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-10-01 15:47:12 +0700

    add script for steps algoritm tutorial

--- script/interactive.js
+++ script/interactive.js
@@ -1,4 +1,4 @@
-// Mengambil elemen judul-proses
+//! Mengambil elemen judul-proses
 let judulProses = document.querySelector('.judul-proses');
 
 // Mengambil elemen isi-proses yang terkait
@@ -12,5 +12,39 @@ judulProses.addEventListener('click', function () {
     } else {
         // Jika tidak aktif, maka aktifkan (display:block)
         isiProses.style.display = 'block';
+    }
+});
+
+
+//! Script untuk key XOR
+// Mengambil elemen select dan input
+let keyTypeSelect = document.getElementById('key-type');
+let keyInput = document.getElementById('key-input');
+
+// Menambahkan event listener pada perubahan nilai select menggunakan event input
+keyTypeSelect.addEventListener('input', function () {
+    // Mendapatkan nilai yang dipilih dalam select
+    let selectedValue = keyTypeSelect.value;
+
+    // Mengubah tipe input berdasarkan nilai yang dipilih
+    if (selectedValue === 'ascii-char') {
+        keyInput.setAttribute('type', 'text');
+    } else if (selectedValue === 'ascii-decimal') {
+        keyInput.setAttribute('type', 'number');
+    }
+});
+
+
+// ! script untuk penjelasan setiap algoritma
+let judulPenjelasan = document.querySelector('.judul-penjelasan');
+let langkahLangkah = document.querySelector('.langkah-langkah');
+
+judulPenjelasan.addEventListener('click', function () {
+    // Jika langkah-langkah aktif, maka nonaktifkan (display:none)
+    if (langkahLangkah.style.display === 'block') {
+        langkahLangkah.style.display = 'none';
+    } else {
+        // Jika tidak aktif, maka aktifkan (display:block)
+        langkahLangkah.style.display = 'block';
     }
 });

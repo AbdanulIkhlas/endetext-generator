26537bb443dad19d154935cdfc8ced5a902d0147
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-10-01 13:29:45 +0700

    create a file to store all the algortima functions

---
+++ allAlgorithmFunction.php
@@ -0,0 +1,110 @@
+<?php 
+// Algortima caesar chipper
+function caesarCipher($text, $key, $action) {
+    $result = "";
+    $key = ($action == 'enkripsi') ? $key : -$key;
+    // jika action=decrypt, $key akan menjadi negatif agar proses dekripsi berfungsi dengan benar
+    // key=key(kunci)
+    for ($i = 0; $i < strlen($text); $i++) {
+        $char = $text[$i];
+        if (ctype_alpha($char)) { // jika huruf alfabet 
+            $isUpperCase = ctype_upper($char); // huruf besar atau kecil
+            $char = strtolower($char); // diubah ke huruf kecil
+            // RUMUS C = (P+K) mod 26 ; P = (C-K) mod 26 
+            $char = chr(((ord($char) - 97 + $key + 26) % 26) + 97);
+            // ord=mengambil kode ASCII dari karakter 
+            // dikurang 97 untuk memudahkan perhitungan (97=kode ASCII huruf a)
+            // tambahkan dengan key atau kunci dan tambahkan 26 dan operasi modulo (%26) untuk memastikan hasil dalam rentang 0-25
+            // ditambah 97 untuk mengembalikan dalam kode ASCII
+            if ($isUpperCase) {
+                $char = strtoupper($char);// jika aslinya huruf besar dikembalikan
+            }
+        } else {
+            $char = $text[$i]; // Jaga karakter non-alphabet seperti spasi atau tanda baca
+        }
+        $result .= $char; //gabungkan karakter
+    }
+
+    return $result;
+}
+
+// Algortima vigenere chipper
+function vigenereCipher($text, $key, $action) {
+    $result = '';
+    $key = strtoupper($key);// diubah ke huruf besar untuk mempermudah penjumlahan
+    $key = preg_replace("/[^a-zA-Z]/", "", $key);// Filter kata kunci hanya dengan huruf alfabet
+    $keyLength = strlen($key);// panjang kunci
+    $keyIndex = 0;
+
+    for ($i = 0; $i < strlen($text); $i++) {
+        $char = $text[$i];
+        if (ctype_alpha($char)) { // jika huruf alfabet 
+            $isLowerCase = ctype_lower($char); // huruf besar atau kecil
+            $char = strtoupper($char);
+            $shift = ord($key[$keyIndex]) - 65;
+            // ord=mengambil kode ASCII dari karakter 
+            // dikurang 65 untuk memudahkan perhitungan (65=kode ASCII huruf A)
+            if ($action === 'deskripsi') {
+                $shift = -$shift;// jika action=decrypt, $shift akan menjadi negatif agar proses dekripsi berfungsi dengan benar
+            }
+            // RUMUS C = (P+K) mod 26 ; P = (C-K) mod 26 
+            $char = chr(((ord($char) - 65 + $shift + 26) % 26) + 65);
+            // ord=mengambil kode ASCII dari karakter
+            // dikurangkan 65 untuk mengubah indeks ke rentang 0-25
+            // tambahkan dengan shift atau kunci dan tambahkan 26 dan operasi modulo (%26) untuk memastikan hasil dalam rentang 0-25
+            // ditambah 65 untuk mengembalikan dalam kode ASCII
+
+            if ($isLowerCase) {
+                $char = strtolower($char);// jika aslinya huruf kecil dikembalikan
+            }
+
+            $keyIndex = ($keyIndex + 1) % $keyLength;
+            // karakter kunci berikutnya, jika sudah mencapai panjang kata kunci, maka akan kembali ke 0 untuk diulang dengan modulo
+        }
+        $result .= $char;
+    }
+
+    return $result;
+}
+
+
+//  Algoritma XOR
+function xorCipher($text) {
+    $result = '';
+    $resultb = '';
+    for ($i = 0; $i < strlen($text); $i++) {
+        $char = $text[$i];
+        $keyChar = '';
+        
+        // Memilih jenis kunci berdasarkan pilihan pengguna
+        if ($_POST["key-type"] === "ascii-char") {
+            $keyChar = $_POST["key-char"];
+        } elseif ($_POST["key-type"] === "ascii-decimal") {
+            $keyChar = chr($_POST["key-decimal"]);
+        }
+
+        // RUMUS C = P XOR K ; P = C XOR K
+        $resultChar = chr(ord($char) ^ ord($keyChar));
+        // operasi XOR dalam bentuk ASCII dan diubah kembali menjadi karakter
+        
+        $result .= $resultChar;
+
+        // Konversi hasil ke biner
+        $resultBinary = '';
+        for ($j = 0; $j < strlen($resultChar); $j++) {
+            $resultBinary .= str_pad(decbin(ord($resultChar[$j])), 8, '0', STR_PAD_LEFT) . ' ';
+            // decbin() mengonversi kode ASCII tersebut ke dalam representasi biner
+            // str_pad(..., 8, '0', STR_PAD_LEFT) menambahkan nol pada awal biner jika panjang biner kurang dari 8 digit.
+        }
+        
+        // semua hasil biner
+        $resultb .= $resultBinary;
+        returnResultBinary($resultb);
+    }
+    return $result;
+}
+
+function returnResultBinary($result){
+    return $result;
+}
+?>
